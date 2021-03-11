71be2639f0a1cda782c0ccc3b7b183878e327636
Avgustin Gochev <44026609+gochev99@users.noreply.github.com>
2021-03-12 00:24:28 +0200

    Update main.js

--- week1/calculator/main.js
+++ week1/calculator/main.js
@@ -7,66 +7,37 @@ document.onkeydown = function(e) {
    }
    else if(e.key === "Enter") {
        calc();
-       document.querySelector('#input-1').focus();
+       document.querySelector('#input-2').focus();
    }
 };
 
-function func() {
-
-    const input1 = document.querySelector('#input-1');
-    const res = document.querySelector('#input-res');
-
-    if(input1.value.length == 0) {
-        res.value = '';
-        return;
-    }
-    var inputs = input1.value.split(' ');
-    if(inputs.length == 1 || inputs.length == 2) {
-        var number = +inputs[0];
-        if(isNaN(number)) return;
-        res.value = number;
-    }
-    else if(inputs.length == 3 && inputs[2].length !== 0) {
-        switch(inputs[1]) {
-            case '+': res.value = +inputs[0] + +inputs[2]; break;
-            case '-': res.value = +inputs[0] - +inputs[2]; break;
-            case '*': res.value = +inputs[0] * +inputs[2]; break;
-            case '/': res.value = +inputs[0] / +inputs[2]; break;
-            case '^': res.value = Math.pow(+inputs[0], +inputs[2]); break;
-            case '%': res.value = +inputs[0] % +inputs[2]; break;
-        }
-    }
-    res.style.width = res.value.length + 1 + 'ch';
-}
-
 function calc() {
     const input1 = document.querySelector('#input-1');
+    const input2 = document.querySelector('#input-2');
+    const operation = document.querySelector('#my-select');
 
-    
-    var inputs = input1.value.split(' ');
-    
-    if(inputs[2] == 0 && inputs[1] == '/') {
+    if(input2.value == 0 && operation.value == 'divide') {
         alert('You cannot divide by 0!');
         return;
     }
 
-    switch(inputs[1]) {
-        case '+': input1.value = +inputs[0] + +inputs[2]; break;
-        case '-': input1.value = +inputs[0] - +inputs[2]; break;
-        case '*': input1.value = +inputs[0] * +inputs[2]; break;
-        case '/': input1.value = +inputs[0] / +inputs[2]; break;
-        case '^': input1.value = Math.pow(+inputs[0], +inputs[2]); break;
-        case '%': input1.value = +inputs[0] % +inputs[2]; break;
+    switch(operation.value) {
+        case 'plus': input1.value = +input1.value + +input2.value; break; // Same as Number(input1.value)
+        case 'minus': input1.value = +input1.value - +input2.value; break;
+        case 'mult': input1.value = +input1.value * +input2.value; break;
+        case 'divide': input1.value = +input1.value / +input2.value; break;
     }
+    input2.value = '';
 
-    input1.focus();
 }
 
+// It doesn't work if the function's name is clear() :? maybe a saved word?
 function clr() {
     const input1 = document.querySelector('#input-1');
-    const input2 = document.querySelector('#input-res');
+    const input2 = document.querySelector('#input-2');
+    const operation = document.querySelector('#my-select');
 
-    input1.value = '';
-    input2.value = '';
-    input1.focus();
+    input1.value = 0;
+    input2.value = 0;
+    operation.value = 'plus';
 }
